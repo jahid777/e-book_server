37c2b3121e9fe0b5c568f1db298a97a5e9405eb8
md zihan <za0162hid@gmail.com>
2022-04-11 20:48:53 +0600

    nodeset up

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7adfu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const port = process.env.PORT || 5000;
-// const port = 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
